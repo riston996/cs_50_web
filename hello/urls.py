8ff7d92022ae398aa84bf66ceb9ba6144b08c82b
Riston <ristonthomas@gmail.com>
2023-10-08 09:49:37 +0400

    django 2

--- hello/urls.py
+++ hello/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path("", views.home, name="home"),
+    path("home", views.home, name="home"),
+    path("<str:name>", views.greet, name="greet"),
     path("brian", views.brian, name="brian"),
     path("david", views.david, name="david")
 ]
--- hello/views.py
+++ hello/views.py
@@ -1,10 +1,14 @@
 from django.http import HttpResponse
 
+
 def home(request):
-    return HttpResponse("Hello,riston")
+    return render(request, "hello/home.html")
 
 def brian(request):
     return HttpResponse("Hello, Brian")
 
 def david(request):
     return HttpResponse("Hello, David")
+
+def greet(request, name):
+    return HttpResponse(f"Hello, {name.capitalize()}!")
